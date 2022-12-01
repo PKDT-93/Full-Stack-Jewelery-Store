7554a97cf93b0bdfb8a612e2f7a53c811f8850c7
Maitry Rohit <101202431+MaitryRohit@users.noreply.github.com>
2022-11-30 20:15:47 -0700

    Updated some database stuff

--- jewelryDatabase/jewelryDatabase/models.py
+++ jewelryDatabase/jewelryDatabase/models.py
@@ -8,131 +8,62 @@
 from django.db import models
 
 
+class Bracelet(models.Model):
+    itemid = models.OneToOneField('Item', on_delete = models.CASCADE, db_column='ItemID', related_name = '%(class)s_ItemID')  # Field name made lowercase.
+    braceletbarcode = models.OneToOneField('Item', on_delete = models.CASCADE, db_column='BraceletBarcode', related_name = '%(class)s_Barcode')  # Field name made lowercase.
+
+    class Meta:
+        db_table = 'Bracelet'
+
+
 class Item(models.Model):
     itemid = models.AutoField(db_column='ItemID', primary_key=True)  # Field name made lowercase.
     barcode = models.IntegerField(db_column='Barcode', unique=True)  # Field name made lowercase.
-    weight = models.IntegerField(db_column='Weight')  # Field name made lowercase.
-    price = models.IntegerField(db_column='Price')  # Field name made lowercase.
+    weight = models.IntegerField(db_column='Weight', blank=True, null=True)  # Field name made lowercase.
+    price = models.IntegerField(db_column='Price', blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
-        managed = False
         db_table = 'Item'
 
 
-class Pendant(models.Model):
+class Necklace(models.Model):
+    itemid = models.ForeignKey(Item, on_delete = models.CASCADE, db_column='ItemID',related_name = '%(class)s_ItemID')  # Field name made lowercase.
+    ringbarcode = models.OneToOneField(Item, on_delete = models.CASCADE, db_column='RingBarcode', related_name = '%(class)s_Barcode')  # Field name made lowercase.
+
+    class Meta:
+        db_table = 'Necklace'
+
+
+class Other(models.Model):
     itemid = models.ForeignKey(Item, on_delete = models.CASCADE, db_column='ItemID', related_name = '%(class)s_ItemID')  # Field name made lowercase.
+    otherbarcode = models.OneToOneField(Item, on_delete = models.CASCADE, db_column='OtherBarcode', related_name = '%(class)s_Barcode')  # Field name made lowercase.
+    type = models.TextField(db_column='Type', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        db_table = 'Other'
+
+
+class Pendant(models.Model):
+    itemid = models.ForeignKey(Item, on_delete = models.CASCADE, db_column='ItemID',related_name = '%(class)s_ItemID')  # Field name made lowercase.
     pendantbarcode = models.OneToOneField(Item, on_delete = models.CASCADE, db_column='PendantBarcode', related_name = '%(class)s_Barcode')  # Field name made lowercase.
 
     class Meta:
-        managed = False
         db_table = 'Pendant'
 
 
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=150)
+class Ring(models.Model):
+    itemid = models.ForeignKey(Item, on_delete = models.CASCADE, db_column='ItemID', related_name = '%(class)s_ItemID')  # Field name made lowercase.
+    ringbarcode = models.OneToOneField(Item, on_delete = models.CASCADE, db_column='RingBarcode',related_name = '%(class)s_Barcode')  # Field name made lowercase.
 
     class Meta:
-        managed = False
-        db_table = 'auth_group'
+        db_table = 'Ring'
 
 
-class AuthGroupPermissions(models.Model):
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
-        unique_together = (('group', 'permission'),)
-
-
-class AuthPermission(models.Model):
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-    name = models.CharField(max_length=255)
+class Watch(models.Model):
+    itemid = models.ForeignKey(Item, on_delete = models.CASCADE, db_column='ItemID',related_name = '%(class)s_ItemID')  # Field name made lowercase.
+    watchbarcode = models.OneToOneField(Item, on_delete = models.CASCADE, db_column='WatchBarcode', related_name = '%(class)s_Barcode')  # Field name made lowercase.
+    watchbrand = models.TextField(db_column='WatchBrand')  # Field name made lowercase.
+    batterytype = models.TextField(db_column='BatteryType')  # Field name made lowercase.
 
     class Meta:
-        managed = False
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
-
-
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.BooleanField()
-    username = models.CharField(unique=True, max_length=150)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.BooleanField()
-    is_active = models.BooleanField()
-    date_joined = models.DateTimeField()
-    first_name = models.CharField(max_length=150)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user'
-
-
-class AuthUserGroups(models.Model):
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
-        unique_together = (('user', 'group'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user', 'permission'),)
-
-
-class DjangoAdminLog(models.Model):
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.PositiveSmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    action_time = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
+        db_table = 'Watch'
